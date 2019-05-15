4bda9c80c42df20455c4293d958ed7a4b7b71bbc
Eduardo Castro <eduardocastro.jj@gmail.com>
2019-05-15 13:58:46 -0300

    Update sbt plugin structure

--- src/main/scala/jasperreports/JasperReportsKeys.scala
+++ src/main/scala/jasperreports/JasperReportsKeys.scala
@@ -8,6 +8,6 @@ object JasperReportsKeys {
   lazy val jasperReportsCompile = taskKey[Unit]("Generates jasper files using jrxml files")
   lazy val jasperReportsCleanOutputFiles = taskKey[Unit]("Cleans jasper files")
 
-  lazy val jasperReportsInputPath = settingKey[String]("Directory where jrxml files will be read")
-  lazy val jasperReportsOutputPath = settingKey[String]("Directory where jasper files will be written")
+  lazy val jasperReportsInputFolder = settingKey[String]("Directory where jrxml files will be read")
+  lazy val jasperReportsOutputFolder = settingKey[String]("Directory where jasper files will be written")
 }
--- src/main/scala/jasperreports/JasperReportsSbtPlugin.scala
+++ src/main/scala/jasperreports/JasperReportsSbtPlugin.scala
@@ -1,12 +1,7 @@
 package jasperreports
 
-import java.io.File
-
-import net.sf.jasperreports.engine.JasperCompileManager
 import sbt.Keys._
 import sbt.{Def, _}
-
-import scala.util.{Failure, Success, Try}
 
 class JasperReportsSbtPlugin extends AutoPlugin {
   val GroupId = "com.jeduardocosta.jasperreports"
@@ -22,43 +17,8 @@ class JasperReportsSbtPlugin extends AutoPlugin {
       jasperReportsInstance := new JasperReports(streams.value.log),
 
       jasperReportsCompile := jasperReportsInstance.value.compileFiles(
-        jasperReportsInputPath.value, jasperReportsOutputPath.value),
+        jasperReportsInputFolder.value, jasperReportsOutputFolder.value),
 
-      jasperReportsCleanOutputFiles := jasperReportsInstance.value.cleanFiles(jasperReportsOutputPath.value))
-  }
-
-  private def performCompileFiles(path: String): Unit = {
-    getFilesFromPath(path)
-      .filter(_.getName.contains("jrxml"))
-      .foreach(compileJasperFile)
-  }
-
-  private def performCleanOutputFiles(path: String): Unit = {
-    val files = getFilesFromPath(path)
-    IO.delete(files)
-  }
-
-  private def compileJasperFile(file: File): Unit = {
-    val jasperPath = getClass.getClassLoader.getResource(file.getAbsolutePath)
-    val jasperCompiledFileName = file.getName.replace(".jrxml", ".jasper")
-    val jasperCompiledPath = s"$jasperReportsOutputPath/$jasperCompiledFileName"
-
-    Try(JasperCompileManager.compileReportToFile(jasperPath.getPath, jasperCompiledPath)) match {
-      case Success(_) =>
-        //streams.value.log.debug(s"[sbt-jasper] The jasper file was generated from xmlPath: $jasperPath")
-      case Failure(error) =>
-        //streams.value.log.err(s"[sbt-jasper] Failure to generate jasper file from xmlPath: $jasperPath, error: ${error.getStackTrace.mkString}")
-    }
-  }
-
-  private def getFilesFromPath(path: String): List[File] = {
-    Try(new File(path)) match {
-      case Success(file) =>
-        if (file.exists && file.isDirectory) file.listFiles.filter(_.isFile).toList
-        else List.empty
-      case Failure(_) =>
-        //streams.value.log.warn(s"[sbt-jasper] It was not possible to extract files from path=$path")
-        List.empty
-    }
+      jasperReportsCleanOutputFiles := jasperReportsInstance.value.cleanFiles(jasperReportsOutputFolder.value))
   }
 }
